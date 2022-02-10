310b36fd909abb4cdc759dc619efa9d8875fee5b
Bujdoso Attila <bujdosoattila60@gmail.com>
2022-02-10 14:55:39 +0100

    induló állapot

---
+++ hello2.cpp
@@ -0,0 +1,12 @@
+// hello2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout << "Hello World!\n"; 
+    std::cout << "Ez az első változat";
+}
